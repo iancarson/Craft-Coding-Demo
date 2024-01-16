6c8c86e9eb3abdc91ebd1516eba53fc239f14f00
Ian Carson <38280903+iancarson@users.noreply.github.com>
2024-01-15 22:38:38 -0500

    Update DBInitializer.java

--- untitled/src/main/resources/DBInitializer.java
+++ untitled/src/main/resources/DBInitializer.java
@@ -18,7 +18,7 @@ public class DBInitializer {
 
     private static final String fileName = "/Users/alpsendil/src/player/src/main/resources/players.csv";
 
-    // comment out so the values are not loaded more than once in D
+    // comment out so the values are not loaded more than once in 
 //    @Bean
 //    CommandLineRunner initializeDatabase(PlayerRepository repository) {
 //
